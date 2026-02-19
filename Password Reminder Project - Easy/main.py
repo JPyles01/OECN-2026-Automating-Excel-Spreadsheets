7749b5bf0b1e0341f970a88e4fd451bde8441a95
JPyles01 <jon@noacsc.org>
2026-02-19 11:41:00 -0500

    2-19-26 Creation of project
    Created project with the easy example first

---
+++ Password Reminder Project - Easy/main.py
@@ -0,0 +1,8 @@
+#how to start project in visual studio code
+#1. open visual studio code
+#2. open the folder where you have the project
+#3. open the terminal in visual studio code (you can do this by clicking on the terminal tab at the bottom of the screen)
+#4. in the terminal, type "python run.py" and press enter
+
+
+print("test")
